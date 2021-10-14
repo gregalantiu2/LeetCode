709f0cec7e2ccbcbfdb2d2602b5f11c79e189bc0
School <School@Gregs-MBP.lan>
2021-10-13 23:38:20 -0400

    started removenumber

---
+++ RemoveNumber/RemoveNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RemoveNumber
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[] { 3,2,2,3 };
+
+            Solution.RemoveElement(array,3);
+        }
+    }
+    public static class Solution
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int[] newArray = new int[nums.Length];
+            int counter = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != val)
+                {
+                    newArray[i] = nums[i];
+                    counter++;
+                }
+            }
+            nums = newArray;
+            return counter;
+        }
+    }
+}
