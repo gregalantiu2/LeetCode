25f13c795b841ce79db56f84885def80d9aacc50
School <School@Gregs-MBP.lan>
2021-10-14 22:37:04 -0400

    WIP

---
+++ LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LongestCommonPrefix
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Solution.LongestCommonPrefix(new string[] { "flower", "flow", "flight" });
+        }
+    }
+    public static class Solution
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string result = "";
+            bool match = true;
+
+            for(int i = 0; i < strs.Length; i++)
+            {
+                if()
+            }
+
+            while(match)
+            {
+                if()
+                {
+
+                }
+                else
+                {
+                    match = false;
+                }
+            }
+            return result;
+        }
+    }
+}
