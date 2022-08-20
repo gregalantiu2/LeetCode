fcaba26336ed914dae30bf9fd7f3f86a3d7b656b
Tiu, Gregory Alan <gtiu@gatech.edu>
2022-08-19 22:31:28 -0400

    Added in code to run console, now logic is left to do

--- TwoSum/TwoSum/Program.cs
+++ TwoSum/TwoSum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace TwoSum
 {
@@ -6,7 +8,45 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Enter in some numbers separated by commas");
+            var numbers = Console.ReadLine();
+            
+            Console.WriteLine("Enter in a target sum");
+            var target = Int32.Parse(Console.ReadLine());
+
+            numbers.Split(",").ToList();
+
+            int[] realNumbers = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                realNumbers[i] = Int32.Parse(realNumbers[i].ToString());
+            }
+
+            var result = twoSum(realNumbers, target);
+
+            string stringResult = "[";
+            
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i != result.Length - 1)
+                {
+                    stringResult += result[i].ToString() + ",";    
+                }
+                else
+                {
+                    stringResult += result[i].ToString();
+                }
+            }
+
+            stringResult += "]";
+            
+            Console.WriteLine(stringResult);
+        }
+
+        public static int[] twoSum(int[] nums, int target)
+        {
+            return new int[] { 1 , 2};
         }
     }
 }
