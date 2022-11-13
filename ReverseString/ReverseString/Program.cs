ee8198bb05859ec161b4ab50a43cfa510fabd303
Tiu, Gregory Alan <gtiu@gatech.edu>
2022-11-13 00:01:04 -0500

    v1 done

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter in a string");
+            var input = Console.ReadLine();
+            Console.WriteLine(Program.ReverseString(input.ToCharArray()));
+        }
+
+        public static char[] ReverseString(char[] s)
+        {
+            Array.Reverse(s);
+            string result = "";
+            foreach (var character in s)
+            {
+                result += character;
+            }
+            return s;
+        }
+    }
+}
