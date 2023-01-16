b30b075cdc7010e15752e36961f82c0f9a465f0e
Tiu, Gregory Alan <gtiu@gatech.edu>
2023-01-16 02:11:52 -0500

    Addtwonumbers started

---
+++ Add_Two_Numbers/Add_Two_Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Add_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.AddTwoNumbers();
+        }
+        
+        static void AddTwoNumbers() {
+            
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
