e15bff4c807d41212c8cc08ccd09078dee6c2784
Greg Tiu <tiugreg2@gmail.com>
2023-12-20 18:40:09 -0500

    ConcatenateArray done

---
+++ ArrayQuestionTemplate/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] nums = new int[6] {0,2,1,5,3,4};
+
+Console.WriteLine(Solution.CheckAnswer(Solution.Solution_Implementation(nums)).ToString());
+
+public static class Solution {
+    public static int[] Solution_Implementation(int[] nums) {
+        int[] answer = new int[nums.Length];
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            answer[i] = nums[nums[i]];
+        }
+
+        return answer;
+    }
+
+    public static bool CheckAnswer(int[] solution)
+    {
+        return solution.SequenceEqual(new int[] {0,1,2,4,5,3});
+    }
+}
---
+++ ConcatenationOfArray/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums = new int[3] {1,2,1};
+
+Console.WriteLine(Solution.CheckAnswer(Solution.GetConcatenation(nums)).ToString());
+
+public static class Solution {
+    public static int[] GetConcatenation(int[] nums) {
+        int n = nums.Length;
+        int[] ans = new int[n * 2];
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+
+    public static bool CheckAnswer(int[] solution)
+    {
+        return solution.SequenceEqual(new int[] {1,2,1,1,2,1});
+    }
+}
